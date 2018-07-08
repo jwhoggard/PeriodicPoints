2054e2cc87a43fb1e4da1cf660830defa85fc7df
John W. Hoggard <jwhoggard@gmail.com>
2018-07-08 18:38:05 -0400

    Fix updated file.

--- Window.cpp
+++ Window.cpp
@@ -13,13 +13,6 @@ Cell* Cell::GetConnection(size_t i) {
   }
 }
 
-/*
-  Given an (i, j) index in the grid, convert it to the real-number
-  coordinates of that point.
-
-  Assumes that the indices are valid, although invalid coordinates
-  will still return a reasonable (x, y) coordinate pair.
-*/
 std::pair<double, double> Window::GetCoordinatesFromIndex(int i, int j) {
   double x, y;
   x = leftX + (0.5+i)*width/xDiv;
@@ -27,17 +20,6 @@ std::pair<double, double> Window::GetCoordinatesFromIndex(int i, int j) {
   return std::make_pair(x, y);
 }
 
-/*
-  Given an (x, y) real-number coordinate pair, will return the (i, j)
-  coordinates in the window's grid which contains that point.
-
-  Cells are inclusive on left and bottom, so are of the form
-  [x, x+d)x[y, y+d)
-
-  If the resulting point is outside the grid, will return the
-  coordinates (-1, -1).  The pair (-1, -1) will be considered to be
-  pointing to the "dead" cell.
- */
 std::pair<int, int> Window::GetIndexFromCoordinates(double x, double y) {
   int i, j;
   if(x<leftX or y<lowerY or x >= leftX+width or y >= lowerY + height) {
